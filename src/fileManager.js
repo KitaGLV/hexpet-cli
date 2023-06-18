b39f2fe3c4edee0d52ce0086ef74d63b4e047add
Nikita Gulyaev <mikitka.glv@gmail.com>
2023-06-18 17:36:32 +0300

    Add functions for create and read hexpet.json

---
+++ src/fileManager.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+
+const filePath = 'hexpet.json';
+
+function createDefaultJSON(path){
+  const defaultJson = JSON.stringify({tasks: []});
+  fs.writeFileSync(path, defaultJson);
+}
+
+function getParseJSON(path){
+  const data = fs.readFileSync(path, 'utf8');
+  return JSON.parse(data);
+}
+
+// Test
+
+createDefaultJSON(filePath);
+console.log(getParseJSON(filePath));
