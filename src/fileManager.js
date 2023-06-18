c99e95caa8f43abff2fa2b1c7affe564ccea9aef
Nikita Gulyaev <mikitka.glv@gmail.com>
2023-06-18 17:52:16 +0300

    Fix eslint errors

--- src/fileManager.js
+++ src/fileManager.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 
 const filePath = 'hexpet.json';
 
-function createDefaultJSON(path){
-  const defaultJson = JSON.stringify({tasks: []});
+function createDefaultJSON(path) {
+  const defaultJson = JSON.stringify({ tasks: [] });
   fs.writeFileSync(path, defaultJson);
 }
 
-function getParseJSON(path){
+function getParseJSON(path) {
   const data = fs.readFileSync(path, 'utf8');
   return JSON.parse(data);
 }
