fd5931a583f9f3c2244c9ac45b52594631d91a39
Nikita Gulyaev <mikitka.glv@gmail.com>
2023-06-29 19:07:08 +0300

    Add timer prototype

--- src/timer.js
+++ src/timer.js
@@ -1,1 +1,31 @@
-// timer
+const minutes = 25;
+const duration = minutes * 60;
+
+let secondsRemaining = duration;
+
+function onTimerEnd() {
+  console.log('\nВремя вышло!');
+}
+
+function formatTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = seconds % 60;
+  return `${hours}:${mins < 10 ? '0' : ''}${mins}:${secs < 10 ? '0' : ''}${secs}`;
+}
+
+function updateTimer() {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  process.stdout.write(`Осталось ${formatTime(secondsRemaining)}`);
+  secondsRemaining -= 1;
+  if (secondsRemaining >= 0) {
+    setTimeout(updateTimer, 1000);
+  } else {
+    onTimerEnd();
+  }
+}
+
+setTimeout(updateTimer, 0);
+
+console.log(`Таймер запущен на ${minutes} минут`);
